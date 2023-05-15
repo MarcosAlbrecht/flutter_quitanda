5a6a21c6e07ab402fe6d61d93235b6b40f34dcec
unknown <marquinhos.beto@hotmail.com>
2023-05-15 17:49:45 -0300

    create ordertab and ordertile

---
+++ lib/src/pages/orders/components/order_tile.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+import 'package:quitanda_app/src/models/cart_item_model.dart';
+import 'package:quitanda_app/src/models/order_model.dart';
+import 'package:quitanda_app/src/services/utils_services.dart';
+
+class OrderTile extends StatelessWidget {
+  final OrderModel order;
+
+  OrderTile({
+    Key? key,
+    required this.order,
+  }) : super(key: key);
+
+  final UtilServices utilServices = UtilServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          dividerColor: Colors.transparent,
+        ),
+        child: ExpansionTile(
+          title: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Pedido: ${order.id}'),
+              Text(
+                utilServices.formatDateTime(order.createdDateTime),
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+          children: [
+            SizedBox(
+              height: 150,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: ListView(
+                      children: order.items.map(
+                        (orderItem) {
+                          return _OrderItemWidget(
+                            utilServices: utilServices,
+                            orderItem: orderItem,
+                          );
+                        },
+                      ).toList(),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(color: Colors.blue),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _OrderItemWidget extends StatelessWidget {
+  const _OrderItemWidget({
+    Key? key,
+    required this.utilServices,
+    required this.orderItem,
+  }) : super(key: key);
+
+  final UtilServices utilServices;
+
+  final CartItemModel orderItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Text(
+            '${orderItem.quantity} ${orderItem.item.unit} ',
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: Text(orderItem.item.itemName),
+          ),
+          Text(
+            utilServices.priceToCurrency(
+              orderItem.totalPrice(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/src/services/utils_services.dart
+++ lib/src/services/utils_services.dart
@@ -1,4 +1,5 @@
 import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class UtilServices {
   //R$ VALOR
@@ -8,5 +9,11 @@ class UtilServices {
     );
 
     return numberFormat.format(price);
+  }
+
+  String formatDateTime(DateTime dateTime) {
+    initializeDateFormatting();
+    DateFormat dateFormat = DateFormat.yMd('pt_BR').add_Hm();
+    return dateFormat.format(dateTime);
   }
 }
