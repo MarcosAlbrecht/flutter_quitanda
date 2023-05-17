ce08c3fee1a56c0282bc3c3e7a373e2fd0e2fb79
unknown <marquinhos.beto@hotmail.com>
2023-05-17 08:59:53 -0300

    final commit screens dev

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -2,7 +2,6 @@ import 'package:add_to_cart_animation/add_to_cart_animation.dart';
 import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:quitanda_app/src/config/custom_colors.dart';
 import 'package:quitanda_app/src/pages/common_widgets/custom_shimmer.dart';
 import 'package:quitanda_app/src/pages/home/components/category_tile.dart';
